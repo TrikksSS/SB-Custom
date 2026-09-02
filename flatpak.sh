1d12df7ed721a8dbc44635ac4678a83a8c8ab2d3
TrikksSS <TrikksSS>
2026-09-02 13:43:41 -0400

    flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,48 +1,49 @@
+
 #!/bin/bash
 
 echo "This script will install the Flathub repo and add a currated list of flatpaks. Please enter your password"
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-color_echo "yellow" "Installing Firefox(Flatpak)"
+echo -e "\e[33mInstalling Firefox\e[0m"
 flatpak install -y flathub org.mozilla.firefox
-color_echo "purple" "Firefox Installed"
+echo  "Firefox Installed"
 
-color_echo "red" "Installing Edge"
+echo "Installing Edge"
 flatpak install -y flathub com.microsoft.Edge
-color_echo "purple" "You have been edged"
+echo  "You have been edged"
 
-color_echo "purple" "Installing ProtonVPN"
+echo  "Installing ProtonVPN"
 flatpak install -y flathub com.protonvpn.www
-color_echo "green" "ProtonVPN Installed"
+echo  "ProtonVPN Installed"
 
-color_echo "purple" "Minecraft Time"
+echo  "Minecraft Time"
 flatpak install -y flathub org.prismlauncher.PrismLauncher
-color_echo "Cyan" "Mined"
+echo  "Mined"
 
-color_echo "purple" "Installing the boomer player"
+echo  "Installing the boomer player"
 flatpak install -y flathub info.smplayer.SMPlayer
-color_echo "Red" "Installed"
+echo "Installed"
 
-color_echo "purple" "Installing Whatsie.."
+echo  "Installing Whatsie.."
 flatpak install -y flathub com.ktechpit.whatsie
-color_echo "purple" "Whatsie Installed!!!"
+echo  "Whatsie Installed!!!"
 
-color_echo "yellow" "Installing Flatseal..."
+echo  "Installing Flatseal..."
 flatpak install -y flathub com.github.tchx84.Flatseal
-color_echo "green" "Flatseal installed successfully."
+echo "Flatseal installed successfully."
 
-color_echo "yellow" "Installing Extension Manager..."
+echo "Installing Extension Manager..."
 flatpak install -y flathub com.mattjakeman.ExtensionManager
-color_echo "green" "Extension Manager installed successfully."
+echo "Extension Manager installed successfully."
 
-color_echo "yellow" "Installing Bottles..."
+echo "Installing Bottles..."
 flatpak install -y flathub com.usebottles.bottles
-color_echo "green" "Bottles installed successfully."
+echo "Bottles installed successfully."
 
-color_echo "yellow" "Installing Deja Dup..."
+echo "Installing Deja Dup..."
 flatpak install -y flathub org.gnome.DejaDup
-color_echo "green" "Deja Dup installed successfully."
+echo "Deja Dup installed successfully."
 
 
-echo "Green" "all pakcages installed"
+echo "All Packages Installed"
